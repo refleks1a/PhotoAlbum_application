ae8d54904c3acfb0543c433e23401962ef4de575
refleks1a <145009608+refleks1a@users.noreply.github.com>
2023-11-28 23:29:00 +0400

    Remove django-secret-key from settings.py

--- photoalbum/photoalbum/settings.py
+++ photoalbum/photoalbum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yg&k3v@p!h6c29hl3n4_m631)!f8v3c)jyo711$p76dohotrfb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
